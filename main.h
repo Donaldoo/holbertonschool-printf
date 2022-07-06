3b0ea12e2641ac0f89fadea68dabfb0d041a2c48
Donaldoo <donald.kumaraku@gmail.com>
2022-07-06 02:49:40 -0700

    Added task

--- main.h
+++ main.h
@@ -1,8 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * Prototypes of cunctions used in this project
+ */
+
 int _putchar(char c);
 int print_char(va_arg);
 int print_string(va_arg);
+int print_int(va_arg);
+int print_float(va_arg);
 
 #endif
