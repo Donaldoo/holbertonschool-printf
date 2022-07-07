5a84dcd248eddf203e4c0cecbb6396b584962234
tizihoxha <tizihoxha@gmail.com>
2022-07-07 10:28:16 -0700

    added

--- print_functions.c
+++ print_functions.c
@@ -38,12 +38,33 @@ int print_string(va_list arg)
 /**
  * print_int - Function to print numbers
  * @arg: list of arguments
- * Return: 1
+ * Return: len
  */
 
 int print_int(__attribute__((unused))va_list arg)
 {
-	return (0);
+	int n, i = 1, len = 0;
+	unsigned int nr;
+
+	n = va_arg(arg, int);
+
+	if (n < 0)
+	{
+		_putchar('-');
+		nr = n * (-1);
+		len++;
+	}
+	else
+		nr = n;
+	while (nr / i > 9)
+		i *= 10;
+	while (i != 0)
+	{
+		len += _putchar(nr / i + '0');
+		nr = nr % i;
+		i /= 10;
+	}
+	return (len);
 }
 
 
