b84fa49299b7d6760ee6faf06b5b68b702bd9678
Donaldoo <donald.kumaraku@gmail.com>
2022-07-08 03:18:35 -0700

    Added task

--- _printf.c
+++ _printf.c
@@ -29,16 +29,12 @@ int _printf(const char *format, ...)
 			{
 				if (format[i + 1] == *print[j].symbol)
 				{
-					len = len + print[j].f(arg);
-					i++;
-					flag = 1;
-				}
+					len = len + print[j].f(arg), i++, flag = 1;
 				j++;
 			}
 			if (flag == 0)
 			{
-				_putchar(format[i]);
-				len += 1;
+				_putchar(format[i]), len += 1;
 			}
 		}
 		else if (format[i] == '%' && format[i + 1] == '%')
